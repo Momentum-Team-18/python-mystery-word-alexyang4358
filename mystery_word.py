3b345d4ede6d41c383d1d2f4c43ba773a888ad70
alexyang4358 <alexyang4358@gmail.com>
2023-05-11 09:36:01 -0400

    open file

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,22 @@
+import random
+
+
 def play_game():
-    pass
+    with open('words.txt') as f:
+        text = f.read()
+        text_full = text.split()
+        selected_word = random.choice(text_full)
+        hyphen_word = selected_word.replace("a-z", "_")
+
+    guesses = 2
+    while guesses > 0:
+        letter = input('Your word is: ' + str(hyphen_word) +
+                       '. Guess first letter: ')
+        if letter in selected_word:
+            print("good job")
+        else:
+            print("nope")
+            guesses -= 1
 
 
 if __name__ == "__main__":
