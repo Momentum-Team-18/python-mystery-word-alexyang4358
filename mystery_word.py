3b19a1d7352a559182645ee5e88592c3fe816451
alexyang4358 <alexyang4358@gmail.com>
2023-05-11 11:04:03 -0400

    open file and display hyphen

--- mystery_word.py
+++ mystery_word.py
@@ -6,17 +6,21 @@ def play_game():
         text = f.read()
         text_full = text.split()
         selected_word = random.choice(text_full)
-        hyphen_word = selected_word.replace("a-z", "_")
+        hyphen_word = '_' * len(selected_word)
+    print(hyphen_word + " " + selected_word)
 
-    guesses = 2
-    while guesses > 0:
-        letter = input('Your word is: ' + str(hyphen_word) +
-                       '. Guess first letter: ')
-        if letter in selected_word:
-            print("good job")
-        else:
-            print("nope")
-            guesses -= 1
+    for i in range(len(selected_word)):
+        print(selected_word[i])
+
+    # guesses = 2
+    # while guesses > 0:
+    #     letter = input('Your word is: ' + str(hyphen_word) +
+    #                    '. Guess first letter: ')
+    #     if letter in selected_word:
+    #         print("good job")
+    #     else:
+    #         print("nope")
+    #         guesses -= 1
 
 
 if __name__ == "__main__":
